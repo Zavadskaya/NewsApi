d16f1e7807ef5e80374d0a23314238a7de774d2f
Ilya Mihneev <ilya.mihneev@external.wirecard.com>
2019-04-22 15:48:37 +0300

    code refactoring and news scroll fixes

--- app/src/main/java/com/example/user/news/ViewHolder/Adapter/ListNewsAdapter.kt
+++ app/src/main/java/com/example/user/news/ViewHolder/Adapter/ListNewsAdapter.kt
@@ -1,6 +1,5 @@
 package com.example.user.news.ViewHolder.Adapter
 
-import android.content.Context
 import android.content.Intent
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
@@ -8,41 +7,46 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.user.news.ArticleActivity
 import com.example.user.news.Interface.ItemClickListener
-import com.example.user.news.Model.Headlines
+import com.example.user.news.Model.Article
 import com.example.user.news.R
 import com.example.user.news.ViewHolder.Holder.ListNewsViewHolder
 import com.example.user.news.net.GlobalUrl
 import com.squareup.picasso.Picasso
 
 
-class ListNewsAdapter(private val context: Context, private val articles: Headlines?): RecyclerView.Adapter<ListNewsViewHolder>() {
-    override fun onCreateViewHolder(p0: ViewGroup, viewType: Int): ListNewsViewHolder {
-        val inflater = LayoutInflater.from(p0!!.context)
-        val itemView = inflater.inflate(R.layout.article_news_layout, p0, false)
-        return ListNewsViewHolder(itemView)
-    }
-    override fun onBindViewHolder(holder: ListNewsViewHolder, position: Int) {
-        holder.article_title.text = articles!!.articles[position].title.toString()
-        Picasso.get()
-                .load(articles.articles[position].urlToImage)
-                .into(holder.imageView)
-        holder.article_data.text = GlobalUrl.DateToTimeFormat(articles!!.articles[position].publishedAt!!.toString())
+class ListNewsAdapter(private val data: List<Article>) : RecyclerView.Adapter<ListNewsViewHolder>() {
 
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ListNewsViewHolder = ListNewsViewHolder(
+        LayoutInflater
+            .from(viewGroup.context)
+            .inflate(R.layout.article_news_layout, viewGroup, false)
+    )
+
+    override fun onBindViewHolder(holder: ListNewsViewHolder, position: Int) {
+        val article: Article = data[position]
+
+        Picasso.get().load(article.urlToImage).into(holder.imageView)
+        holder.article_title.text = article.title.toString()
+        holder.article_data.text = GlobalUrl.DateToTimeFormat(article.publishedAt!!.toString())
+        //FIXME What is publishedAt will be null? ^^
 
         holder.setItemClickListener(object : ItemClickListener {
             override fun onClick(view: View, position: Int) {
-
-                val intent = Intent(context, ArticleActivity::class.java)
-                intent.putExtra("description", this@ListNewsAdapter.articles.articles!![position].description)
-                intent.putExtra("image",this@ListNewsAdapter.articles.articles!![position].urlToImage)
-                intent.putExtra("title",this@ListNewsAdapter.articles.articles!![position].title)
-                intent.putExtra("date",this@ListNewsAdapter.articles.articles!![position].publishedAt)
-                intent.putExtra("author",this@ListNewsAdapter.articles.articles!![position].author)
-                context.startActivity(intent)
+                view.context.let {
+                    it.startActivity(Intent(it, ArticleActivity::class.java).apply {
+                        putExtra("description", article.description)
+                        putExtra("image", article.urlToImage)
+                        putExtra("title", article.title)
+                        putExtra("date", article.publishedAt)
+                        putExtra("author", article.author)
+                    })
+                }
             }
         })
-
     }
-    override fun getItemCount(): Int = articles!!.articles!!.size
+
+    override fun getItemCount(): Int = data.size
+    override fun getItemId(position: Int): Long =
+        data[position].url.hashCode().toLong() //FIXME need to change to data[position].id instead of 'url', if not NULL
 }
 
